c03b1414964f79ac8bb62253c1198053df3a09e9
joujou144 <aznimr@protonmail.com>
2022-01-25 01:33:05 +0800

    edits

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
   return (
     <header>
-        <div className='header-container'>
+        <div className='header-section'>
           <img src="../images/globe.png" alt="globe-logo" className="header-logo"/>
           <h3>my travel journal.</h3>
         </div>
