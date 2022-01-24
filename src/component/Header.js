df8628ef4312f1a66e42f33e0397e6d416aac6d5
joujou144 <aznimr@protonmail.com>
2022-01-24 23:04:01 +0800

    commit

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header() {
+  return (
+    <header>
+        <div className='header-container'>
+          <img src="../images/globe.png" alt="globe-logo" className="header-logo"/>
+          <h3>my travel journal.</h3>
+        </div>
+    </header>
+  )
+}
